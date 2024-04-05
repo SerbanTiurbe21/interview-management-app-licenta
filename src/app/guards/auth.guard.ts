08c904f3bde6c4f95ba35a72ccc69a2cd158c159
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2024-04-05 14:47:49 +0300

    created auth guard and added it to app-routing module

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,28 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+export const canActivate = (
+  router: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+):
+  | boolean
+  | UrlTree
+  | Promise<boolean | UrlTree>
+  | Observable<boolean | UrlTree> => {
+  const authService = inject(AuthService);
+  const route = inject(Router);
+  const isAuthenticated = authService.isAuthenticated();
+  if (isAuthenticated) {
+    return true;
+  } else {
+    return route.createUrlTree(['/login']);
+  }
 };
