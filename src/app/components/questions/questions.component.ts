15aeed1e67417e568861a9f56b05c866919328e9
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2024-04-22 22:22:32 +0300

    adding topics works

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
 import { Question } from 'src/app/interfaces/question.model';
 import { Topic } from 'src/app/interfaces/topic.model';
+import { QuestionsService } from 'src/app/services/questions.service';
+import { TopicService } from 'src/app/services/topic.service';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.css'],
 })
-export class QuestionsComponent {
+export class QuestionsComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
   displayAddTopicDialog: boolean = false;
   newTopicName: string = '';
   questions: Question[] = [];
@@ -19,15 +23,27 @@ export class QuestionsComponent {
 
   constructor(
     private confirmationService: ConfirmationService,
-    private messageService: MessageService
+    private messageService: MessageService,
+    private topicService: TopicService,
+    private questionService: QuestionsService
   ) {}
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
 
   ngOnInit() {
     this.loadTopics();
   }
 
   loadTopics() {
-    // Load topics from the backend
+    this.topicService
+      .displayTopics()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((topics) => {
+        this.topics = topics;
+      });
   }
 
   onTopicChange() {
@@ -58,14 +74,29 @@ export class QuestionsComponent {
   }
 
   saveNewTopic() {
-    // Logic to save the new topic
     this.displayAddTopicDialog = false;
-    this.newTopicName = ''; // Clear the input after saving
-    // make an API call to save the new topic and then reload the topics and display a success message
+    const addedTopic: Topic = {
+      name: this.newTopicName,
+    };
+    this.topicService
+      .addTopic(addedTopic)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: () => {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Topic added successfully',
+          });
+          console.log('Topic added successfully');
+          this.loadTopics();
+        },
+        error: () => {},
+      });
+    this.newTopicName = '';
   }
 
   clearNewTopic() {
-    // Logic to clear the topic input
     this.newTopicName = '';
   }
 }
--- src/app/interfaces/topic.model.ts
+++ src/app/interfaces/topic.model.ts
@@ -1,5 +1,5 @@
 export interface Topic {
   id?: string;
   name: string;
-  nrOfQuestions: number;
+  nrOfQuestions?: number;
 }
